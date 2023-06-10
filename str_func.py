aa405d5cc291b019683f7d3fe0049f62853e046e
Demam1988 <134836925+Demam1988@users.noreply.github.com>
2023-06-10 17:30:07 +0300

    Update str_func.py

--- str_func.py
+++ str_func.py
@@ -1,8 +1,2 @@
 def capital_letters(string):
-    '''Возвращает Все заглавными буквами'''
     return string.upper()
-
-
-def first_capital(string2):
-    '''преобразования в верхний регистр первых букв слов в строке'''
-    return string2.title()
