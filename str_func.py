bed3b4fa569156fd58d3912a396fe19d5842a265
Mamed Mamedov <demam777@gmail.com>
2023-06-10 17:07:20 +0300

    add docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def capital_letters(string):
     return string.upper()
+
+
+def first_capital(string2):
+    '''преобразования в верхний регистр первых букв слов в строке'''
+    return string2.title()
